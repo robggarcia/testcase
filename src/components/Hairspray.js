6dc003070e03aa6c9052b5171e4157c2838bb09b
robggarcia <robert.g.garcia82@gmail.com>
2023-09-22 16:18:23 -0400

    hairspray added to new feature

---
+++ src/components/Hairspray.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hairspray = () => {
+  return (
+    <div>This is a test to see if my branching works as I think it should</div>
+  );
+};
+
+export default Hairspray;
